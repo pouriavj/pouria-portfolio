e87f1120120bfd0b188c7c4a56924d65a918a82d
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-05-31 01:00:49 +0330

    Add FavIcon And Site Manifest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import "./globals.css";
 
@@ -8,8 +8,23 @@ import Footer from "@/components/Footer";
 export const metadata: Metadata = {
   title: "Pouria Portfolio",
   description: "Pouria Vojdani's Personal Portfolio",
+  manifest: "/site.webmanifest",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon-192x192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icon-512x512.png", sizes: "512x512", type: "image/png" },
+    ],
+    apple: {
+      url: "/apple-touch-icon.png",
+      sizes: "180x180",
+      type: "image/png",
+    },
+  },
 };
-export const viewport = {
+export const viewport: Viewport = {
   themeColor: "#1a1a2a",
 };
 export default function RootLayout({
