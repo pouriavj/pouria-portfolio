35bdd40c8f42b191063881178a73d62216046f00
Pouria <108181964+pouriavj@users.noreply.github.com>
2026-06-02 03:59:01 +0330

    Add Sitemap And Robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,8 +3,10 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://www.pouriavojdani.ir/",
+      url: "https://pouriavojdani.ir",
       lastModified: new Date(),
+      priority: 1,
+      changeFrequency: "weekly",
     },
   ];
 }
